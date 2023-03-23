47abf27ecaf1f612577342b46cde975b9ed227bb
stevenbertolucci <bertolus@flip2.engr.oregonstate.edu>
2023-03-23 10:15:46 -0700

    Database Web App

--- database/db-connector.js
+++ database/db-connector.js
@@ -5,9 +5,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_bertolus',  // Remove asterick and replace with your username
-    password        : '2426',         // Replace asterick with your password
-    database        : 'cs340_bertolus'     // Replace asterick with your username
+    user            : 'cs340_*',    // Remove asterick and replace with your username
+    password        : '*',          // Replace asterick with your password
+    database        : 'cs340_*'     // Replace asterick with your username
 })
 
 // Export it for use in our application
